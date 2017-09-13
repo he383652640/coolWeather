b27b1a3274ff79ebd97bac6fd4a27f355b6f2d7f
Churiy <churiy@foxmail.com>
2017-09-13 18:04:14 +0800

    churiy第五次提交代码

--- app/src/main/java/com/example/churiy/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/churiy/coolweather/WeatherActivity.java
@@ -9,5 +9,7 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+
     }
 }
